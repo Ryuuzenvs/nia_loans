07c8a07516b9a06a782bc220ba97e543a51f2a28
ryuuzenvs <dhankaede@gmail.com>
2026-03-02 17:26:11 +0700

    backup com

--- database/migrations/2026_02_22_115141_create_borrowers_table.php
+++ database/migrations/2026_02_22_115141_create_borrowers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
     $table->string('name');
     $table->string('alamat'); 
-    $table->integer('no hp');  
+    $table->integer('no_hp');  
     $table->timestamps();
         });
     }
